20511c6e5defa7cb77acd5c2a3bb2dea6bd2072b
Дмитрий Телегин <teledima00@mail.ru>
2020-08-16 03:21:02 +0500

    preparing wonderful_answer command to add one answer

--- slash_commands.py
+++ slash_commands.py
@@ -1,18 +1,21 @@
 import constants
 import requests
 import json
+import znatoks
+import re
+import gspread.exceptions as errors_sheet
 from flask import Flask, request, make_response, jsonify
-from znatoks import find_user_spreadsheet
 from slack.web.classes import blocks
 from slack.web.client import WebClient
 from slack.errors import SlackApiError
 from tasks import async_task
 
+
 app = Flask(__name__)
 
 
-def ephemeral_message(response_url, text):
-    requests.post(response_url, data=json.dumps({'content_type': 'ephemeral', 'text': text}))
+def ephemeral_message(response_url, text=None, blocks=None):
+    requests.post(response_url, data=json.dumps({'content_type': 'ephemeral', 'text': text, 'blocks': blocks}))
 
 
 def reply(text: str):
@@ -29,24 +32,32 @@ def is_admin(client, req):
     else:
         return False
 
+def check_empty(req):
+    if not req['text']:
+        ephemeral_message(req['response_url'], req['text'])
+
+
+@app.route('/')
+def hello():
+    return "Hello, Slack App!"
+
 
 @app.route('/get_info', methods=['POST'])
 def get_info():
     req = request.form
-    client = WebClient(token=constants.SLACK_OAUTH_TOKEN)
+    client = WebClient(token=constants.SLACK_OAUTH_TOKEN_TEST)
     if not is_admin(client, req):
         return reply("You don't have permission")
-    if not req['text']:
-        return reply('Query is empty')
-    get_info_background(client, request.form)
+    check_empty(req)
+    get_info_background(request.form)
     return reply('Got it!')
 
 
 @async_task
-def get_info_background(client, req):
-    info = find_user_spreadsheet(req['text'])
+def get_info_background(req):
+    info = znatoks.find_user_spreadsheet(req['text'])
     if all(i is None for i in info):
-        ephemeral_message(req['response_url'], 'User does not found')
+        ephemeral_message(req['response_url'], 'User is not found')
         return
     sections = []
     for row in info:
@@ -55,7 +66,46 @@ def get_info_background(client, req):
                 sections.append(
                     blocks.SectionBlock(text=blocks.TextObject(f'_*{key}*_: {row[key]}', 'mrkdwn')).to_dict())
             sections.append(blocks.DividerBlock().to_dict())
-    client.chat_postEphemeral(channel=req['channel_id'], user=req['user_id'], blocks=sections)
+    ephemeral_message(req['response_url'], blocks=sections)
+
+
+@app.route('/wonderful_answer', methods=['POST'])
+def wonderful_answer():
+    req = request.form
+    check_empty(req)
+    wonderful_answer_background(req)
+    return reply('Привет! Сейчас мы немного проверим и посчитаем этот ответ')
+
+
+@async_task
+def wonderful_answer_background(req):
+    params = req['text'].split()
+    url = params[0]
+    who_add_ans = None if len(params) == 1 else params[1]
+    result = znatoks.is_correct_request(url, req['user_id'], who_add_ans)
+    if not result['ok']:
+        if result['cause'] == 'same_user':
+            ephemeral_message(req['response_url'], 'Вы не можете отправлять свои ответы')
+            return
+        elif result['cause'] == 'blocked_error':
+            ephemeral_message(req['response_url'], 'Извините, произошла ошибка при проверке ответа. Попробуйте позже')
+            return
+        elif result['cause'] == 'slack_api_error':
+            ephemeral_message(req['response_url'], f'Ошибка при запросе к Slack API: {result["description"]}')
+            return
+    wonderful_answer_table = znatoks.authorize().open('Copy of Кандидаты').worksheet('wonderful answer')
+    cells = wonderful_answer_table.findall(url, in_column=1)
+    if len(cells) == 1:
+        count_cell = wonderful_answer_table.cell(cells[0].row, cells[0].col+1)
+        wonderful_answer_table.update_cell(count_cell.row, count_cell.col, int(count_cell.value)+1)
+        pass
+    elif len(cells) > 1:
+        for cell in cells:
+            nick_name = wonderful_answer_table.cell(cell.row, cell.col+2)
+        pass
+    else:  # url doesn't exist in the table yet
+        wonderful_answer_table.insert_row([url, 0], znatoks.next_available_row(wonderful_answer_table))
+    ephemeral_message(req['response_url'], 'Answer added!')
 
 
 @app.route('/entry_point', methods=["POST"])
--- znatoks.py
+++ znatoks.py
@@ -1,26 +1,47 @@
+import requests
 import json
-
+import re
 import gspread
 import gspread.exceptions as errors_sheet
 from oauth2client.service_account import ServiceAccountCredentials
+from bs4 import BeautifulSoup
+from slack import WebClient
+import slack.errors as slack_errors
 
-client = gspread.authorize(ServiceAccountCredentials.
-                           from_json_keyfile_name('files/slash-commands-archive-a7072e2bbb96.json',
-                                                  ['https://spreadsheets.google.com/feeds',
-                                                   'https://www.googleapis.com/auth/drive']
-                                                  )
-                           )
-spreadsheet = client.open('Copy of Кандидаты')
+import constants
+
+
+class SameUserError(Exception):
+    pass
+
+
+class BlockedError(Exception):
+    pass
+
+
+def authorize():
+    return gspread.authorize(ServiceAccountCredentials.
+                             from_json_keyfile_name('files/slash-commands-archive-a7072e2bbb96.json',
+                                                    ['https://spreadsheets.google.com/feeds',
+                                                     'https://www.googleapis.com/auth/drive']
+                                                    )
+                             )
+
+
+def next_available_row(worksheet):
+    str_list = list(filter(None, worksheet.col_values(1)))
+    return len(str_list) + 1
 
 
 def find_user_spreadsheet(url):
+    client = authorize()
+    spreadsheet = client.open('Copy of Кандидаты')
     tables_list = ['кандидаты', 'команда знатоков']
-    return [find_in_table(table, url) for table in tables_list]
+    return [find_in_table(spreadsheet.worksheet(table), url) for table in tables_list]
 
 
-def find_in_table(table, url):
+def find_in_table(worksheet, url):
     try:
-        worksheet = spreadsheet.worksheet(table)
         headers_table = []
         for header in worksheet.row_values(1):
             if not header:
@@ -31,3 +52,71 @@ def find_in_table(table, url):
         return dict(zip(headers_table + ['таблица'], values + [worksheet.title]))
     except errors_sheet.CellNotFound:
         pass
+
+
+def get_display_name(user_id):
+    try:
+        info = WebClient(token=constants.SLACK_OAUTH_TOKEN).users_info(user=user_id).data
+        if info['ok']:
+            return info['user']['profile']['display_name']
+    except slack_errors.SlackApiError as e:
+        raise e
+
+
+def is_expert(nick_name):
+    # TODO necessary to split the table 'магистры/архивариусы' on 3 table: 'архивариусы', 'магистры', 'модераторы'
+    return True
+
+
+def check_answer(list_authors, user_add_ans_name, user_ex_com_name):
+    for nick_name in list_authors:
+        # /wonderful_answer url @nick
+        if user_add_ans_name is not None:
+            if nick_name == user_add_ans_name:
+                return {'ok': True}
+        else:  # should check user in expert team and user is not I
+            if len(list_authors) == 1:
+                if nick_name == user_ex_com_name:
+                    return {'ok': False, 'cause': 'same_user'}
+                elif is_expert(nick_name):
+                    return {'ok': True}
+    return {'ok': False, 'cause': 'answer_user_not_found'}
+
+
+def is_correct_request(url, user_ex_com_id, who_add_ans=None):
+    # get info about user executed the command
+    try:
+        user_ex_com_name = get_display_name(user_ex_com_id).lower().strip()
+    except slack_errors.SlackApiError as e:
+        return {'ok': False, 'cause': 'slack_api_error', 'description': e.response['error']}
+
+    if who_add_ans is None:
+        user_add_ans_name = None
+    else:
+        # find user name added answer
+        reg_ex = re.compile(r'\w+\|\w+')
+        if reg_ex.search(who_add_ans) is None:
+            # param just nickname
+            user_add_ans_name = who_add_ans
+        else:
+            # param is slack nickname (@nickname)
+            try:
+                user_add_ans_name = get_display_name(reg_ex.search(who_add_ans).group().split('|')[0]).lower().strip()
+            except slack_errors.SlackApiError:
+                user_add_ans_name = None
+
+    soup = BeautifulSoup(requests.get(url=url, headers=json.load(open('files/headers.json', 'r'))).text, 'lxml')
+    if soup.find('title').text != 'You have been blocked':
+        try:
+            # Here possible AttributeError exception
+            list_authors = [author_description_tag.find('div').find('span').text.lower().strip()
+                            for author_description_tag
+                            in soup.find_all('div', attrs={'class': 'brn-qpage-next-answer-box-author__description'})]
+            return check_answer(list_authors, user_add_ans_name, user_ex_com_name)
+        except AttributeError:
+            list_authors = [author_description_tag.find_all('div')[1].text
+                            for author_description_tag
+                            in soup.find_all('div', attrs={'class': 'brn-kodiak-answer__user'})]
+            return check_answer(list_authors, user_add_ans_name, user_ex_com_name)
+    else:
+        return {'ok': False, 'cause': 'blocked_error'}
