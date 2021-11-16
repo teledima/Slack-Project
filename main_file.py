a0e73b24e2529286e9a6a26111ecd204398dd4b8
teledima <teledima00@gmail.com>
2021-11-16 20:41:42 +0500

    indent refactor

--- main_file.py
+++ main_file.py
@@ -8,16 +8,17 @@ from channels_checker.worker import channels_checker_blueprint
 
 app = Flask(__name__)
 blueprints = [
-  views_endpoint_blueprint, 
-  event_endpoint_blueprint, 
-  auth_blueprint, 
-  smiles_check_blueprint, 
-  znatok_helper_blueprint, 
-  channels_checker_blueprint
+    views_endpoint_blueprint,
+    event_endpoint_blueprint,
+    auth_blueprint,
+    smiles_check_blueprint,
+    znatok_helper_blueprint,
+    channels_checker_blueprint
 ]
 for bp in blueprints:
-  app.register_blueprint(bp)
+    app.register_blueprint(bp)
+
 
 @app.route('/')
 def hello():
-  return "Hello, Slack App!"
+    return "Hello, Slack App!"
